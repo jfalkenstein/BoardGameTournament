6245b37ed4bc226522087e9b5eae48cf64f11872
Jon Falkenstein <jtfalkenstein@gmail.com>
2025-01-09 16:57:34 -0600

    calling it normalized rank

--- gametournament/rank_scorer.py
+++ gametournament/rank_scorer.py
@@ -64,7 +64,7 @@ class RankScorer(BaseScorer):
 class RankFormula(Formula):
     def __init__(self, tournament: Tournament):
         super().__init__(tournament)
-        self._inverse_rank = FormulaValue("Inverse Rank")
+        self._inverse_rank = FormulaValue("Inverse Normalized Rank")
 
         self.expression.set(self._inverse_rank * self.rank_multiplier * self.duration_multiplier)
 
