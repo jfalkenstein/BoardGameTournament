d1c66e15ec1c925788de96f97bfdd13790425263
Jon Falkenstein <jtfalkenstein@gmail.com>
2025-01-09 20:47:40 -0600

    adding autocomplete

---
+++ completions.bash
@@ -0,0 +1,29 @@
+_game_tournament_completion() {
+    local IFS=$'\n'
+    local response
+
+    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _GAME_TOURNAMENT_COMPLETE=bash_complete $1)
+
+    for completion in $response; do
+        IFS=',' read type value <<< "$completion"
+
+        if [[ $type == 'dir' ]]; then
+            COMPREPLY=()
+            compopt -o dirnames
+        elif [[ $type == 'file' ]]; then
+            COMPREPLY=()
+            compopt -o default
+        elif [[ $type == 'plain' ]]; then
+            COMPREPLY+=($value)
+        fi
+    done
+
+    return 0
+}
+
+_game_tournament_completion_setup() {
+    complete -o nosort -F _game_tournament_completion game-tournament
+}
+
+_game_tournament_completion_setup;
+
